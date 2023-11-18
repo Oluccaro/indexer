d50591c3fffc9670092f630711f317f86d32079c
Lucas Henrique Cazionato <lucas-cazionato@ufpr.br>
2023-11-18 19:56:25 -0300

    atualizando

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # indexer
+
+Exemplo de chamada 
+```
+./indexer --freq-word all 10100.txt
+```
+
+Para imprimir as palavras basta descomentar a parte do final do main e as linhas 162 e 163 e comentar as partes que chamam a hash_table
+
--- indexer.c
+++ indexer.c
@@ -8,7 +8,7 @@
 //Hash item HI
 typedef struct HI {
   char *key;
-  unsigned int count;
+  unsigned int *count;
   struct HI *next;
 } HI;
 
@@ -46,10 +46,10 @@ HT *create_hash_table(){
 }
 
 //create pair
-HI *hash_item_pair(const char *key, unsigned int value){
+HI *hash_item_pair(const char *key, unsigned int *value){
   HI * hash_item = malloc(sizeof(HI) * 1);
   hash_item->key = malloc(strlen(key) +1);
-
+  hash_item->count = malloc(sizeof(value));
   strcpy(hash_item->key, key);
   hash_item->count = value;
   hash_item->next = NULL;
@@ -58,14 +58,15 @@ HI *hash_item_pair(const char *key, unsigned int value){
 }
 
 //set item
-void hash_table_set(HT *hash_table, const char *key, unsigned int value){
+void hash_table_set(HT *hash_table, const char *key){
   unsigned int index = hash(key);
-
+  unsigned int *count;
+  *count = 1;
   HI *hash_item = hash_table->item[index];
 
   if (hash_item == NULL){
     //primeira ocorrencia da palavra count = 1;
-    hash_table->item[index] = hash_item_pair(key, 1);
+    hash_table->item[index] = hash_item_pair(key, count);
   }
 
   HI *prev;
@@ -79,10 +80,10 @@ void hash_table_set(HT *hash_table, const char *key, unsigned int value){
     hash_item = prev->next; 
   }
 
-  prev->next = hash_item_pair(key, 1);
+  prev->next = hash_item_pair(key, count);
 }
 
-unsigned int hash_table_get(HT *hash_table, char *key){
+unsigned int hash_table_get(HT *hash_table, const char *key){
   unsigned int index = hash(key);
 
   HI *hash_item = hash_table->item[index];
@@ -93,7 +94,7 @@ unsigned int hash_table_get(HT *hash_table, char *key){
 
   while(hash_item != NULL){
     if(strcmp(hash_item->key, key) == 0){
-      return hash_item->count;
+      return *hash_item->count;
     }
     hash_item = hash_item->next;
   }
@@ -111,16 +112,77 @@ char *tolowerstr(const char *s){
   return ls;
 }
 
+// Function to check if a character is punctuation
+int isPunctuation(char ch) {
+    return (ch == '.' || ch == ',' || ch == ';' || ch == ':' || ch == '!' || ch == '?' || ch == '(' || ch == ')' || ch == '[' || ch == ']' || ch == '{' || ch == '}' || ch == '\"' || ch == '\'');
+}
+
+// Function to check if a character is a space or hyphen
+int isSeparator(char ch) {
+    return (ch == ' ' || ch == '-');
+}
+
 int main(int argc, char const *argv[])
 {
+
   unsigned int i = 0;
   char *freq_word = "--freq-word";
 
   //Searching for number of ocurrencies of word in file
   if(strcmp(argv[1], freq_word) == 0){
-    const char *word = tolowerstr(argv[2]);
-    printf("%s", word);
+    //Getting parameters
+    const char *word_to_find = tolowerstr(argv[2]);
     const char *file_name = argv[3];
+    char ch;
+    char word[32];
+    char wordsArray[100000][32];
+    int wordCount = 0;
+    unsigned int i = 0;
+
+    HT *hash_table = create_hash_table();
+
+    //setting file
+    FILE *file = fopen(file_name, "r");
+
+    if(file == NULL){
+      printf("Erro ao abrir arquivo %s", file_name);
+      return 0;
+    }
+
+    // Read the file character by character
+    while ((ch = fgetc(file)) != EOF) {
+        if(isalpha(ch)) {
+            // Convert the character to lowercase and add it to the current word
+            word[i] = tolower(ch);
+            i++;
+        } else if (i > 1) {
+            word[i] = '\0'; // Null-terminate the word
+            hash_table_set(hash_table, word);
+            //strcpy(wordsArray[wordCount], word);
+            //wordCount++;
+            i = 0; // Reset the index for the next word
+        } else if (isSeparator(ch)) {
+            // If we encounter a space or hyphen, treat it as a separator
+            i = 0;
+        }
+        // Ignore punctuation and reset the index for the next word
+        if (isPunctuation(ch)) {
+            i = 0;
+        }
+    }
+
+    //printf("Palavra: %s | ocorrencias: %d", word_to_find, hash_table_get(hash_table, word_to_find));
+
+    // // Close the file
+    // fclose(file);
+
+    // // Display the words
+    // printf("Words in the file (ignoring words with less than 2 characters and punctuation):\n");
+    // for (int j = 0; j < wordCount; j++) {
+    //     printf("%s\n", wordsArray[j]);
+    // }
+
+    return 0;
   }
 
 }
