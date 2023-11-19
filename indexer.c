2175319342cc782aadaeaf33b52eeaceacafe86f
Lucas Henrique Cazionato <lucas-cazionato@ufpr.br>
2023-11-18 22:07:34 -0300

    adicionando função que gera hash_table dado um arquivo

--- indexer.c
+++ indexer.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 
-#define TABLE_SIZE 100000
+#define TABLE_SIZE 1000000
 
 //Hash item HI
 typedef struct HI {
@@ -15,6 +15,7 @@ typedef struct HI {
 //Hash Table HT
 typedef struct HT {
   HI **item;
+  unsigned long int n_items;
 } HT;
 
 //Hash function
@@ -41,6 +42,7 @@ HT *create_hash_table(){
   for(; i < TABLE_SIZE; i++){
     hash_table->item[i] = NULL;
   }
+  hash_table->n_items = 0;
 
   return hash_table;
 }
@@ -64,6 +66,8 @@ void hash_table_set(HT *hash_table, const char *key){
   if (hash_item == NULL){
     //primeira ocorrencia da palavra count = 1;
     hash_table->item[index] = hash_item_pair(key, 1);
+    hash_table->n_items++;
+    return;
   }
 
   HI *prev = (HI*)malloc(sizeof(HI) * 1);
@@ -76,7 +80,7 @@ void hash_table_set(HT *hash_table, const char *key){
     prev = hash_item;
     hash_item = prev->next; 
   }
-
+  hash_table->n_items++;
   prev->next = hash_item_pair(key, 1);
 }
 
@@ -110,13 +114,51 @@ char *tolowerstr(const char *s){
 }
 
 // Function to check if a character is punctuation
-int isPunctuation(char ch) {
+int is_ponctuation(char ch) {
     return (ch == '.' || ch == ',' || ch == ';' || ch == ':' || ch == '!' || ch == '?' || ch == '(' || ch == ')' || ch == '[' || ch == ']' || ch == '{' || ch == '}' || ch == '\"' || ch == '\'');
 }
 
 // Function to check if a character is a space or hyphen
-int isSeparator(char ch) {
+int is_separator(char ch) {
     return (ch == ' ' || ch == '-');
+}
+
+HT *generate_hash_for_file(HT *hash_table, const char *file_name){
+  FILE *file = fopen(file_name, "r");
+  char ch;
+  char word[40];
+  int wordCount = 0;
+  unsigned int i = 0;
+
+  if(file == NULL){
+    printf("Erro ao abrir arquivo %s", file_name);
+    return 0;
+  }
+
+  // Read the file character by character
+  while ((ch = fgetc(file)) != EOF) {
+      if(isalpha(ch)) {
+          // Convert the character to lowercase and add it to the current word
+          word[i] = tolower(ch);
+          i++;
+      } else if (i > 1) {
+          word[i] = '\0'; // Null-terminate the word
+          hash_table_set(hash_table, word);
+          // strcpy(wordsArray[wordCount], word);
+          // wordCount++;
+          i = 0; // Reset the index for the next word
+      } else if (is_separator(ch)) {
+          // If we encounter a space or hyphen, treat it as a separator
+          i = 0;
+      }
+      // Ignore punctuation and reset the index for the next word
+      if (is_ponctuation(ch)) {
+          i = 0;
+      }
+  }
+  // Close the file
+  fclose(file);
+  return hash_table;
 }
 
 int main(int argc, char const *argv[])
@@ -138,50 +180,41 @@ int main(int argc, char const *argv[])
     unsigned int i = 0;
 
     HT *hash_table = create_hash_table();
-    // HI *hash_item = hash_item_pair(word_to_find, 1);
-    // printf("%s | %d\n", hash_item->key, hash_item->count);
 
-    // hash_table->item[0] = hash_item;
-    // printf("%s | %d\n", hash_table->item[0]->key, hash_table->item[0]->count);
+    hash_table = generate_hash_for_file(hash_table, file_name);
+    // //setting file
+    // FILE *file = fopen(file_name, "r");
 
-    // printf("hash %d", hash(word_to_find));
-    // printf("%p", hash_table->item[hash(word_to_find)]);
+    // if(file == NULL){
+    //   printf("Erro ao abrir arquivo %s", file_name);
+    //   return 0;
+    // }
 
-    // hash_table_set(hash_table, word_to_find);
-    //setting file
-    FILE *file = fopen(file_name, "r");
-
-    if(file == NULL){
-      printf("Erro ao abrir arquivo %s", file_name);
-      return 0;
-    }
-
-    // Read the file character by character
-    while ((ch = fgetc(file)) != EOF) {
-        if(isalpha(ch)) {
-            // Convert the character to lowercase and add it to the current word
-            word[i] = tolower(ch);
-            i++;
-        } else if (i > 1) {
-            word[i] = '\0'; // Null-terminate the word
-            hash_table_set(hash_table, word);
-            // strcpy(wordsArray[wordCount], word);
-            // wordCount++;
-            i = 0; // Reset the index for the next word
-        } else if (isSeparator(ch)) {
-            // If we encounter a space or hyphen, treat it as a separator
-            i = 0;
-        }
-        // Ignore punctuation and reset the index for the next word
-        if (isPunctuation(ch)) {
-            i = 0;
-        }
-    }
+    // // Read the file character by character
+    // while ((ch = fgetc(file)) != EOF) {
+    //     if(isalpha(ch)) {
+    //         // Convert the character to lowercase and add it to the current word
+    //         word[i] = tolower(ch);
+    //         i++;
+    //     } else if (i > 1) {
+    //         word[i] = '\0'; // Null-terminate the word
+    //         hash_table_set(hash_table, word);
+    //         // strcpy(wordsArray[wordCount], word);
+    //         // wordCount++;
+    //         i = 0; // Reset the index for the next word
+    //     } else if (is_separator(ch)) {
+    //         // If we encounter a space or hyphen, treat it as a separator
+    //         i = 0;
+    //     }
+    //     // Ignore punctuation and reset the index for the next word
+    //     if (is_ponctuation(ch)) {
+    //         i = 0;
+    //     }
+    // }
 
     printf("Palavra: %s | ocorrencias: %d\n", word_to_find, hash_table_get(hash_table, word_to_find));
-
     // Close the file
-    fclose(file);
+
     free(hash_table);
     // // Display the words
     // printf("Words in the file (ignoring words with less than 2 characters and punctuation):\n");
